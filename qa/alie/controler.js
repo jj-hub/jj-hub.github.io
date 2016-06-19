25f1bafb630366c61adff0aa85061205387a7b91
jj-hub <cocas259@hotmail.com>
2016-06-19 21:22:16 +0100

    2016-06-19 21:22

--- qa/alie/controler.js
+++
@@ -1,9 +0,0 @@
-var phonecatApp = angular.module('phonecatApp', []);
-
-phonecatApp.controller('PhoneListCtrl', function ($scope, $http) {
-  $http.get('v/view_1').success(function(data) {
-    $scope.phones = data;
-  });
-
-  $scope.orderProp = 'price_new';
-});
